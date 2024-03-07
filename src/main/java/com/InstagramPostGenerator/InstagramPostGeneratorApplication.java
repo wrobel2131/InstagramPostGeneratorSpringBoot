4c461482db34b8aa950da293fa3bebad68bc5387
wrobel2131 <wrobelek2131@wp.pl>
2024-03-07 15:38:49 +0100

    init commit

---
+++ src/main/java/com/InstagramPostGenerator/InstagramPostGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.InstagramPostGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramPostGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramPostGeneratorApplication.class, args);
+	}
+
+}
