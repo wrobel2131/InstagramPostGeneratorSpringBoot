a4284cd41a69fa07d3bdf945d1947adaf54bbc26
wrobel2131 <wrobelek2131@wp.pl>
2024-04-16 00:01:32 +0200

    Changes in testController

--- ResourceServer/src/main/java/com/InstagramPostGenerator/ResourceServer/Controller/TestController.java
+++ ResourceServer/src/main/java/com/InstagramPostGenerator/ResourceServer/Controller/TestController.java
@@ -1,13 +1,19 @@
 package com.InstagramPostGenerator.ResourceServer.Controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/resource")
+@Slf4j
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String testEndpoint() {
+
+        log.info("ENDPOINT LOG");
         return "Test endpoint works";
     }
 }
